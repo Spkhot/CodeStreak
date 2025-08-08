339d00a856435f9a6085b35ba6d04af9f6669127
soham khot <sohamkhot75@gmail.com>
2025-08-09 03:08:47 +0530

    improve message sender1

--- backend/models/User.js
+++ backend/models/User.js
@@ -54,7 +54,6 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  joinCode: { type: String, unique: true },
   googleId: String,
   isVerified: { type: Boolean, default: false },
   verifyToken: String,
