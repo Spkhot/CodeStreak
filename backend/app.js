0fdb11f35699e09b128e7bb971b4f57b43f1c262
soham khot <sohamkhot75@gmail.com>
2025-08-14 13:04:14 +0530

    fix error of app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
